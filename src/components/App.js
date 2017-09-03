5bb2242c66c4aa2dcb623bc71a83b22a98cac851
Dore Mioto <dore.mioto@gmail.com>
2017-09-03 22:03:38 +0300

    Added bootstrap navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link, IndexLink } from 'react-router';
+import Navigation from './navigation/Navigation';
 
 // This is a class-based component because the current
 // version of hot reloading won't hot reload a stateless
@@ -9,12 +9,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <IndexLink to="/">Home</IndexLink>
-        {' | '}
-        <Link to="/fuel-savings">Demo App</Link>
-        {' | '}
-        <Link to="/about">About</Link>
-        <br/>
+        <Navigation/>
         {this.props.children}
       </div>
     );
